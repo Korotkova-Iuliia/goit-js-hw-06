fc017b7a25c7309989aa8aeb94926dbb68f36057
Korotkova-Iuliia <3589900@gmail.com>
2021-10-12 15:18:17 +0300

    task 04

--- js/task-04.js
+++ js/task-04.js
@@ -3,18 +3,20 @@ const decBtn = document.querySelector('button[data-action="decrement"]');
 console.log(decBtn);
 const incBtn = document.querySelector('button[data-action="increment"]');
 console.log(incBtn);
+const countClickEl = document.querySelector('#value');
+console.log(countClickEl);
 
 decBtn.addEventListener('click', onDecBtnClick);
 function onDecBtnClick(event) {
   console.log(event.currentTarget.value);
   counterValue -= 1;
   console.log(counterValue);
-  return counterValue;
+  //   return (countClickEl += counterValue);
 }
 incBtn.addEventListener('click', onIncBtnClick);
 function onIncBtnClick(event) {
   console.log(event.currentTarget.value);
   counterValue += 1;
   console.log(counterValue);
-  return counterValue;
+  //   return (countClickEl += counterValue);
 }
