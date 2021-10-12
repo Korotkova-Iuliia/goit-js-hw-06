077bc88eb884777c5aaf890b52092805cd285b9c
Korotkova-Iuliia <3589900@gmail.com>
2021-10-12 23:40:52 +0300

    task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,21 @@
 const inputBtn = document.querySelector('input#validation-input');
+
+const inputDataLengthEl = Number(inputBtn.getAttribute('data-length'));
+const borderInputBtnValid = document.querySelector('style#validation-input');
+
 console.log(inputBtn);
+console.log(borderInputBtnValid);
+console.log(inputBtn.getAttribute('data-length'));
+console.log(inputDataLengthEl);
 
 //
 inputBtn.addEventListener('blur', onInputBtnBlur);
-function onInputBtnBlur() {
-  if (event.currentTarget.value.length === 6) {
+function onInputBtnBlur(event) {
+  if (event.currentTarget.value.length === inputDataLengthEl) {
+    inputBtn.style.borderColor = borderInputBtnValid;
     console.log('fyj');
   }
+  console.log(inputDataLengthEl);
 
   console.log('эsgaeraerhэ');
   console.log(event.currentTarget.value.length);
