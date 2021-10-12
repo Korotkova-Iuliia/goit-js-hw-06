a04d7aa6d1c9361befae46df05f9609c7e4a0831
Korotkova-Iuliia <3589900@gmail.com>
2021-10-13 01:03:26 +0300

    task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,18 @@
 const inputBtn = document.querySelector('input#validation-input');
-
 const inputDataLengthEl = Number(inputBtn.getAttribute('data-length'));
-const borderInputBtnValid = document.querySelector('style#validation-input');
+const borderInputBtnValid = document.querySelector('#validation-input');
 
-console.log(inputBtn);
-console.log(borderInputBtnValid);
-console.log(inputBtn.getAttribute('data-length'));
-console.log(inputDataLengthEl);
-
-//
 inputBtn.addEventListener('blur', onInputBtnBlur);
 function onInputBtnBlur(event) {
   if (event.currentTarget.value.length === inputDataLengthEl) {
-    inputBtn.style.borderColor = borderInputBtnValid;
-    console.log('fyj');
+    return event.currentTarget.classList.add('valid');
   }
   console.log(inputDataLengthEl);
-
-  console.log('эsgaeraerhэ');
-  console.log(event.currentTarget.value.length);
+  event.currentTarget.classList.add('invalid');
+  //    console.log(event.currentTarget.value.length);
 }
+
+// console.log(inputBtn);
+// console.log(borderInputBtnValid);
+// console.log(inputBtn.getAttribute('data-length'));
+// console.log(inputDataLengthEl);
