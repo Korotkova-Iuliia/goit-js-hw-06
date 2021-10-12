9cde3cbbe53c2a4fd7e8ff6ac582e5ec2b53bd6f
Korotkova-Iuliia <3589900@gmail.com>
2021-10-12 21:16:31 +0300

    task 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,11 @@
 const inputBtn = document.querySelector('input#name-input');
-console.log(inputBtn);
 const output = document.querySelector('span#name-output');
-console.log(output);
 
 inputBtn.addEventListener('input', onInputBtnInput);
+
 function onInputBtnInput(event) {
+  if (event.currentTarget.value === '') {
+    return (output.textContent = 'Anonymous');
+  }
   output.textContent = event.currentTarget.value;
 }
