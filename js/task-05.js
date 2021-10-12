55a1a9b1900dc199a7f4d90917f4f2d361851d32
Korotkova-Iuliia <3589900@gmail.com>
2021-10-12 20:20:36 +0300

    task 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,9 @@
-const input = document.querySelector('input#name-input');
-console.log(input);
+const inputBtn = document.querySelector('input#name-input');
+console.log(inputBtn);
 const output = document.querySelector('span#name-output');
 console.log(output);
+
+inputBtn.addEventListener('input', onInputBtnInput);
+function onInputBtnInput(event) {
+  output.textContent = event.currentTarget.value;
+}
