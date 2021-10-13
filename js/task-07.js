6c9a75557f26d490bbe2bd2dae87d299c00f3550
Korotkova-Iuliia <3589900@gmail.com>
2021-10-13 20:44:20 +0300

    task 07

--- js/task-07.js
+++ js/task-07.js
@@ -1,21 +1,16 @@
 const inputBtn = document.querySelector('input#font-size-control');
 const textString = document.querySelector('span#text');
-const valueMinInputBtn = inputBtn.getAttribute('min');
-const valueMaxInputBtn = inputBtn.getAttribute('max');
-console.log(valueMinInputBtn);
-console.log(valueMaxInputBtn);
+// const valueMinInputBtn = inputBtn.getAttribute('min');
+// const valueMaxInputBtn = inputBtn.getAttribute('max');
+// console.log(valueMinInputBtn);
+// console.log(valueMaxInputBtn);
 inputBtn.addEventListener('change', onInputBtnChange);
 
 console.log(inputBtn);
 console.log(textString);
 
-function onInputBtnChange(event) {
-  const values = [];
-  console.log(values);
-  for (let i = 16; i <= 96; i += 1) {
-    values.push(i);
-  }
-  textString.style.fontSize = values;
+function onInputBtnChange() {
+  const inputBtnValue = inputBtn.value;
 
-  //   console.log(event.nextElementSibling.values);
+  textString.style.fontSize = `${inputBtnValue}px`;
 }
