604dd2b5ef9e9bcdc55c0649817394c2f026c65d
Korotkova-Iuliia <3589900@gmail.com>
2021-10-15 18:25:47 +0300

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -2,7 +2,17 @@ const form = document.querySelector('form.login-form');
 console.log(form);
 form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  console.log(formData);
+  formData.forEach((value, name) => {
+    console.log(name);
+    if (value === '' || name === '') {
+      alert('все поля должны быть заполнены');
+    }
+    console.log(value);
+    ('dhjdtyjdtykfSRgEG');
+  });
   console.log(event.currentTarget.elements.email.value);
   event.currentTarget.elements.email.value;
-  event.preventDefault();
 }
