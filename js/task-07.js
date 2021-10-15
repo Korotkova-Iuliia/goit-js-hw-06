eca2b8ed9088f31ef0f05606d917dabdfb6f6370
Korotkova-Iuliia <3589900@gmail.com>
2021-10-15 11:55:04 +0300

    task 07

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputBtn = document.querySelector('input#font-size-control');
 const textString = document.querySelector('span#text');
 
-inputBtn.addEventListener('change', onInputBtnChange);
+inputBtn.addEventListener('input', onInputBtnChange);
 
 function onInputBtnChange() {
   textString.style.fontSize = `${inputBtn.value}px`;
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,8 @@
-
+const form = document.querySelector('form.login-form');
+console.log(form);
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  console.log(event.currentTarget.elements.email.value);
+  event.currentTarget.elements.email.value;
+  event.preventDefault();
+}
