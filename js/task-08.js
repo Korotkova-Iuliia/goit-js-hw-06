3fbc18198cd0eace70b374537f594fa1b7240f40
Korotkova-Iuliia <3589900@gmail.com>
2021-10-16 17:43:13 +0300

    task 10

--- js/task-08.js
+++ js/task-08.js
@@ -5,12 +5,16 @@ function onFormSubmit(event) {
   event.preventDefault();
   const formData = new FormData(event.currentTarget);
   //   console.log(formData);
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    return alert('все поля должны быть заполнены');
+  }
+  const userData = {};
   formData.forEach((value, name) => {
-    if (value === '' || name === '') {
-      return alert('все поля должны быть заполнены');
-    }
-    console.log({ name, value });
+    userData[name] = value;
   });
-
+  console.log(userData);
   form.reset();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('button[data-create]');
+console.log(createBtn);
+const destroyBtn = document.querySelector('button[data-destroy]');
+console.log(destroyBtn);
+
+// const dishes = document.querySelectorAll('.dishes > li');
+// dishes.forEach((dish) => {
+//   console.log(dish.dataset.id);
+// });
+// div#boxes
+// function createBoxes(amount) {
+//   const addContainerEl = document.createElement('div');
+//     const addContainerEl = document.createElement('div');
+
+//   addContainerEl.classList.add('item');
+//   <div></div>;
+// }
