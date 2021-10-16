eecbf385d6dbbc14d2a9dd1deec9731e2c0f54ee
Korotkova-Iuliia <3589900@gmail.com>
2021-10-16 08:20:34 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColorBtn = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  const nameColor = document.querySelector('.color');
+  console.dir((nameColor.textContent = getRandomHexColor()));
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
