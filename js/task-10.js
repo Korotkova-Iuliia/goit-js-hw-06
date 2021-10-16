41c5ea5487404d5c883cad74f9e60be328f88ffb
Korotkova-Iuliia <3589900@gmail.com>
2021-10-16 18:02:00 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -6,7 +6,25 @@ const createBtn = document.querySelector('button[data-create]');
 console.log(createBtn);
 const destroyBtn = document.querySelector('button[data-destroy]');
 console.log(destroyBtn);
+const controlsContainer = document.querySelector('#controls');
+console.log(controlsContainer);
+const inputBtn = controlsContainer.firstElementChild;
+console.log(inputBtn);
+const boxesContainer = document.querySelector('#boxes');
+console.log(boxesContainer);
 
+inputBtn.addEventListener('input', onIcreateBtnInput);
+function onIcreateBtnInput(event) {
+  console.log(Number(event.currentTarget.value));
+
+  // if (event.currentTarget.value.length === inputDataLengthEl) {
+  //   return event.currentTarget.classList.add('valid');
+  // }
+
+  // console.log(inputDataLengthEl);
+  // event.currentTarget.classList.add('invalid');
+  //    console.log(event.currentTarget.value.length);
+}
 // const dishes = document.querySelectorAll('.dishes > li');
 // dishes.forEach((dish) => {
 //   console.log(dish.dataset.id);
