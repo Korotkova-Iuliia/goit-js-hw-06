a2973029710b9ab372b7498578ebcc4cc8efeb6f
Korotkova-Iuliia <3589900@gmail.com>
2021-10-18 09:41:20 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,46 +3,46 @@ function getRandomHexColor() {
 }
 
 const createBtn = document.querySelector('button[data-create]');
-// console.log(createBtn);
 const destroyBtn = document.querySelector('button[data-destroy]');
-// console.log(destroyBtn);
 const controlsContainer = document.querySelector('#controls');
-// console.log(controlsContainer);
 const inputBtn = controlsContainer.firstElementChild;
-// console.log(inputBtn);
 const boxesContainer = document.querySelector('#boxes');
-console.log(boxesContainer);
-
+// console.log(boxesContainer);
 const boxes = boxesContainer.children;
 console.log(boxes);
-
-function getSize() {
-  for (let i = 0; i < boxes.length; i++) {
-    let sizeBoxes;
-    sizeBoxes = `${Math.floor(30 + indexOf(boxes) * 10)}px`;
-    console.log(indexOf(boxes));
-
-    return;
-  }
-}
+console.log(boxes.length);
 inputBtn.addEventListener('input', onInput);
 
 function onInput(event) {
-  const numberBoxes = Number(event.currentTarget.value);
-  console.log(numberBoxes);
+  const amountBoxes = Number(event.currentTarget.value);
+  console.log(amountBoxes);
 
   createBtn.addEventListener('click', onCreateBtnClick);
   const shapeEl = document.createElement('div');
 
-  function onCreateBtnClick(event) {
+  function onCreateBtnClick() {
     shapeEl.style.backgroundColor = getRandomHexColor();
-    shapeEl.style.width = `${getSize()}px`;
-    shapeEl.style.height = '30px';
-    console.log(shapeEl);
+
+    for (let i = 0; i < amountBoxes; i++) {
+      console.log(amountBoxes);
+      let sizeBoxes;
+      sizeBoxes = `${Math.floor(30 + amountBoxes * 10)}px`;
+      shapeEl.style.width = sizeBoxes;
+      shapeEl.style.height = sizeBoxes;
+
+      // console.log(indexOf(boxes));
+      console.log('sgawrg');
+
+      // return;
+    }
     boxesContainer.appendChild(shapeEl);
 
-    console.log(boxes);
-    console.log(boxes.length);
     inputBtn.value = '';
+    return;
   }
+}
+destroyBtn.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  boxesContainer.remove();
+  return;
 }
